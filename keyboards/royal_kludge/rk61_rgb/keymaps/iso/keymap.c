5b22afcc94741fefb7c38d37be8f12450afdd09b
Nosen92 <59702101+Nosen92@users.noreply.github.com>
2026-08-31 00:05:51 +0200

    Fix hyphen and up arrow keymap.c

--- keyboards/royal_kludge/rk61_rgb/keymaps/iso/keymap.c
+++ keyboards/royal_kludge/rk61_rgb/keymaps/iso/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬────┤Ent│
      * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │ #  │   │
      * ├──────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴────┴───┤
-     * │Shift │\ │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ ↑ │  Shift   │
+     * │Shift │\ │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │  Shift   │
      * ├────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┴───┴┬────┬────┤
      * │Ctrl│GUI │Alt │         Space          │Alt │APP │Ctr │ Fn │
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,                KC_ENT,
         KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_NUHS,
-        KC_LSFT,    KC_NUBS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_UP,      KC_RSFT,
+        KC_LSFT,    KC_NUBS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
         KC_LCTL,    KC_LGUI,    KC_LALT,                                        KC_SPC,                                         KC_RALT,    KC_APP,     KC_RCTL,    MO(WINFN)
     ),
     /* Win FN
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,     KC_BRIU,    KC_BRID,    KC_TASK,    KC_FLXP,    RM_VALD,    RM_VALU,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,    KC_PSCR,    KC_SCRL,    KC_PAUS,    _______,    _______,    RM_VALD,                RM_VALU,    RM_NEXT,
         _______,    _______,    KC_MAC,     _______,    _______,    _______,    KC_INS,     KC_HOME,    KC_PGUP,    _______,    RM_SPDD,    RM_SPDU,    _______,
-        _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_DEL,     KC_END,     KC_PGDN,    RM_HUEU,    KC_SLSH,    MO(FNS),
+        _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_DEL,     KC_END,     KC_PGDN,    RM_HUEU,    KC_UP,      MO(FNS),
         QK_BOOT,    _______,    _______,                                        _______,                                        KC_LEFT,    KC_DOWN,    KC_RIGHT,   _______
     ),
     /* Mac Base */
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,                KC_ENT,
         KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_NUHS,
-        KC_LSFT,    KC_NUBS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_UP,      KC_RSFT,
+        KC_LSFT,    KC_NUBS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
         KC_LCTL,    KC_LALT,    KC_LCMD,                                        KC_SPC,                                         KC_RCMD,    KC_RALT,    KC_RCTL,    MO(MACFN)
     ),
     /* Mac FN
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,     KC_BRIU,    KC_BRID,    KC_MCTL,    KC_LPAD,    RM_VALD,    RM_VALU,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,    KC_PSCR,    KC_SCRL,    KC_PAUS,    _______,    _______,    RM_VALD,                RM_VALU,    RM_NEXT,
         _______,    KC_WIN,     _______,    _______,    _______,    _______,    KC_INS,     KC_HOME,    KC_PGUP,    _______,    RM_SPDD,    RM_SPDU,    _______,
-        _______,    _______,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_MPRV,    KC_MNXT,    KC_DEL,     KC_END,     KC_PGDN,    RM_HUEU,    KC_SLSH,    MO(FNS),
+        _______,    _______,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_MPRV,    KC_MNXT,    KC_DEL,     KC_END,     KC_PGDN,    RM_HUEU,    KC_UP,      MO(FNS),
         QK_BOOT,    _______,    _______,                                        _______,                                        KC_LEFT,    KC_DOWN,    KC_RIGHT,   _______
     ),
     /* Fn + Right Shift: F-row */
